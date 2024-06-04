67e75a87be8a2403cdcd62bde871111d78eb4f72
Darby J <codequeenactivated@gmail.com>
2024-06-04 03:11:42 -0400

    add functions

--- RockPaperScissorsProj/app.js
+++ RockPaperScissorsProj/app.js
@@ -1,1 +1,43 @@
-console.log("Hello World")
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+    const choice = options[Math.floor(Math.random() * options.length)]
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerselection == "paper" && computerSelection == "rock")
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+
+}
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "It's a Tie!"
+    }
+    else if(result == "Player"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+function game(){
+    console.log("Welcome!")
+    for (let i = 0; i < 5; i++) {
+         const playerSelection = "rock";
+         const computerSelectiion = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+}
